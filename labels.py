e49b0521ba2a2a976fb23e641b65b2815ed282a6
emilrib <127395067+emilrib@users.noreply.github.com>
2025-03-07 16:32:10 +0100

    add/data_cleaning_process

---
+++ labels.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+from nltk.corpus import stopwords
+
+file_path = "/Users/emiliaribeiro/Documents/Masters/Thesis/sarcoma_treatment_thesis/dataset_clean.csv"
+def import_file(file_path):
+    df = pd.read_csv(file_path)
+    #print(df.head())
+    return df
+df = import_file(file_path)
+
+#print(df.head(3))
+
