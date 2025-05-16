4e8667881b87a3c41506acab9a125adfe3ca8e19
emilrib <127395067+emilrib@users.noreply.github.com>
2025-05-16 11:05:45 +0200

    update/models

--- Evaluation/Bootstrap.py
+++ Evaluation/Bootstrap.py
@@ -23,7 +23,7 @@ X = pd.get_dummies(X, drop_first=True)
 # ---------------------------
 # Train/Test Split
 # ---------------------------
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # ---------------------------
 # Bagged Trees with OOB and Test Error
